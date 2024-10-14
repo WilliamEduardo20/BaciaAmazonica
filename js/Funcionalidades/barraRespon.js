fd888548a00608fb3890c311844c9221263b9530
WilliamEduardo20 <williameduardominorelli@gmail.com>
2024-10-14 13:11:41 -0300

    Voltou ao Normal

--- js/Funcionalidades/barraRespon.js
+++ js/Funcionalidades/barraRespon.js
@@ -12,42 +12,39 @@ export function barraResponsivaPC() {
     ];
     const navBar = document.getElementById('nav-bar');
 
-    // Função para mudar a barra
     function mudaBarra() {
         if (!opaco) {
-            box.classList.add('addOp', 'opacty');
-            button.classList.add('transition', 'opacty');
+            box.classList.add('addOp');
+            box.classList.add('opacty');
+            button.classList.add('transition');
+            button.classList.add('opacty');
             button.classList.remove('button-29');
-
             // Usar setTimeout para adicionar a classe de redirecionamento após a transição
             setTimeout(() => {
+                // Aumenta a barra e espera antes de mostrar os elementos
                 navBar.style.transition = "0.8s ease";
-                navBar.style.height = "200px"; // Aumenta a altura
-                navBar.style.width = "100px";  // Mantém a largura
-
+                navBar.style.width = "200px";
+                navBar.style.height = "100px";
                 liBtn.forEach(elemento => {
                     elemento.classList.remove('redirection');
-                    elemento.style.display = 'block'; // Exibe os elementos em coluna
                 });
             }, 500); // Tempo igual ao da transição
-
             opaco = true;
         } else {
             // Diminuir a altura da barra e esperar antes de esconder os elementos
-            navBar.style.height = "85px"; 
-            navBar.style.removeProperty('width'); 
-
+            navBar.style.height = "85px";
+            navBar.style.removeProperty('width');
             // Usar setTimeout para remover a classe de redirecionamento após a transição
             setTimeout(() => {
                 liBtn.forEach(elemento => {
                     elemento.classList.add('redirection');
-                    elemento.style.display = 'none'; // Oculta os elementos
                 });
                 button.classList.add('button-29');
-                button.classList.remove('opacty', 'transition');
-                box.classList.remove('opacty', 'addOp');
+                button.classList.remove('opacty');
+                button.classList.remove('transition');
+                box.classList.remove('opacty');
+                box.classList.remove('addOp');
             }, 800); // Tempo igual ao da transição
-
             opaco = false;
         }
     }
